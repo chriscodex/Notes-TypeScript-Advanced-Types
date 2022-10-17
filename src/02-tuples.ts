4d2342f368a5c5b98ff68d2c28c01bea1b37abd2
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-17 18:36:30 -0500

    destructuración de una tupla finalizada

--- src/02-tuples.ts
+++ src/02-tuples.ts
@@ -3,10 +3,15 @@ const prices : (number | string)[] = [1,2,3, 'four'];
 prices.push(5);
 
 // Tuple
-let user : [string, number];
-user = ['christian', 1];
+let user : [string, number, boolean];
+user = ['christian', 1, true];
 
 /* Invalid Options */
 // user = [];
 // user = ['christ'];
-// user = ['christ',2,true]
+// user = ['christ',2, true, 2]
+
+/* Destructuración de una tupla */
+const [username, age] = user;
+console.log(username);
+console.log(age);
