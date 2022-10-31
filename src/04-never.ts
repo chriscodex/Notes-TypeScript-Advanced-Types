294c0a02412326866760a712a49673c884eb318f
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-31 07:04:41 -0500

    example function created

--- src/04-never.ts
+++ src/04-never.ts
@@ -7,3 +7,9 @@ const withoudEnd = () => {
 const fail = (message: string) => {
     throw new Error(message);
 }
+
+const example = (input: unknown) => {
+    if (input === 'string') {
+        return 'It is a string'
+    }
+}
