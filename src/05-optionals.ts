7a063bb91a5c01424d510a50082ebcbd23aa1026
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-01 07:32:20 -0500

    function with error created

--- src/05-optionals.ts
+++ src/05-optionals.ts
@@ -9,3 +9,18 @@ export const createProduct = (
         isNew
     }
 }
+
+/* Optinals parameters always are the last parameter
+This example is not possible
+export const createProduct = (
+    id: string | number,
+    stock?: number,
+    isNew: boolean
+) => {
+    return {
+        id,
+        stock,
+        isNew
+    }
+}
+ */
