9777db45740f86b465567a4590498256548eb72d
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-02 07:48:11 -0500

    Other example error added

--- src/05-optionals.ts
+++ src/05-optionals.ts
@@ -25,6 +25,25 @@ export const createProduct = (
 }
  */
 
+/* Problems can happen if we use | instead of ?
+export const createProduct = (
+    id: string | number,
+    isNew?: boolean,
+    stock?: number,
+) => {
+    return {
+        id,
+        stock: stock || 0,
+        isNew: isNew || true
+    }
+}
+
+Is js we have this behaviors
+0 = false
+'' = false
+false = false
+ */
+
 const p1 = createProduct(1,true);
 console.log(p1);
 
