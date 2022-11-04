013891d5542bdd6e4a46f5f884a1292fc880400b
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-04 15:26:20 -0500

    parseStr function modified

--- src/08-overload.ts
+++ src/08-overload.ts
@@ -1,14 +1,15 @@
 
-type customType = string | string[]
+export type customType = string | string[]
 
 function parseStr(input: string): string[];
 function parseStr(input: string[]): string;
 
-function parseStr(input: string | string[]) : unknown {
+function parseStr(input: unknown) : unknown {
     if (typeof input === 'string') {
         return input.split('')
+    } else if (Array.isArray(input)){
+        input.join('')
     }
-    return input.join('')
 }
 
 const rta = parseStr('christ');
@@ -16,4 +17,12 @@ console.log(rta);
 
 const rta2 = parseStr(['c','h','r','i','s']);
 console.log(rta2);
+
+// Other way using type
+function parseStrV2(input: string | string[]) : unknown {
+    if (typeof input === 'string') {
+        return input.split('')
+    }
+    return input.join('')
+}
 
