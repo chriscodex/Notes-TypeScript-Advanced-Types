b600d634c20be0b2771632e19edecb5cdf67eab6
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-04 16:35:51 -0500

    product type created

--- src/09-interfaces.ts
+++ src/09-interfaces.ts
@@ -1,9 +1,22 @@
 type Sizes = 'S' | 'M' | 'L' | 'XL'
 
-type Products = {
+type Product = {
     id: string | number
     title: string
     createdAt: Date
     stock: number
     size: Sizes
 }
+
+let products: Product[] = []
+
+const p1: Product = {
+    id: 1,
+    title: 'product 1',
+    createdAt: new Date(),
+    stock: 10,
+    size: 'M'
+}
+
+products.push(p1);
+console.log(products);
