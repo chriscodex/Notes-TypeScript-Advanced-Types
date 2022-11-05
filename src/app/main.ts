f110a6c926b1b72b22ac6cbc7e37792bbba9aabc
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-05 16:54:16 -0500

    main.ts finished

---
+++ src/app/main.ts
@@ -0,0 +1,11 @@
+import { Product} from './products/product.model'
+
+const p1: Product = {
+    id: '1',
+    title: 'p1',
+    createdAt: new Date(),
+    stock: 4,
+    size: 'S'
+}
+
+console.log(p1)
