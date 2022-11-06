7d9da38fec5d56fb432e2e0d066688c5a8573616
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-06 18:17:35 -0500

    baseModel imported in order interface

--- src/app/orders/order.model.ts
+++ src/app/orders/order.model.ts
@@ -1,5 +1,6 @@
 import { Product } from "../products/product.model";
 import { User } from "../../01-enum";
+import { BaseModel } from "../base.model";
 
 export interface Order {
     id: string | number
