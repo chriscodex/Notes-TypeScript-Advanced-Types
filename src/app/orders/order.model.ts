da44ab36615681958b70c9562a4340cc7e4b5ed1
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-06 17:10:04 -0500

    interface order created

--- src/app/orders/order.model.ts
+++ src/app/orders/order.model.ts
@@ -1,2 +1,9 @@
 import { Product } from "../products/product.model";
 import { User } from "../../01-enum";
+
+export interface Order {
+    id: string | number
+    createdAt: Date
+    products: Product[]
+    user: User
+}
