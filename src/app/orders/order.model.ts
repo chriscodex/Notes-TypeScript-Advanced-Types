b8fdec935c2243744750bfdce147cc2dc4ae5cee
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-06 18:18:09 -0500

    order interface now extends from baseModel

--- src/app/orders/order.model.ts
+++ src/app/orders/order.model.ts
@@ -2,9 +2,7 @@ import { Product } from "../products/product.model";
 import { User } from "../../01-enum";
 import { BaseModel } from "../base.model";
 
-export interface Order {
-    id: string | number
-    createdAt: Date
+export interface Order extends BaseModel {
     products: Product[]
     user: User
 }
