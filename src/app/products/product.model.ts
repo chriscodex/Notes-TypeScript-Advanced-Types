a4df174b89bc18637a1730df92190445f3490868
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-07 18:19:53 -0500

    product interface modified

--- src/app/products/product.model.ts
+++ src/app/products/product.model.ts
@@ -3,10 +3,8 @@ import { BaseModel } from '../base.model'
 
 export type Sizes = 'S' | 'M' | 'L' | 'XL'
 
-export interface Product {
-    id: string | number
+export interface Product extends BaseModel{
     title: string
-    createdAt: Date
     stock: number
     size: Sizes
     category: Category
