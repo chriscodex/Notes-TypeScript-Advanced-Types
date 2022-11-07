088ac432c333e631ec2227ff2d2eba81d4586a3a
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-07 18:21:13 -0500

    size property of product interface now it is optional

--- src/app/products/product.model.ts
+++ src/app/products/product.model.ts
@@ -6,6 +6,6 @@ export type Sizes = 'S' | 'M' | 'L' | 'XL'
 export interface Product extends BaseModel{
     title: string
     stock: number
-    size: Sizes
+    size?: Sizes
     category: Category
 }
