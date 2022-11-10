ba8a763c0367b6d970e79ea722531234b1d95d86
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-10 02:42:15 -0500

    faker library imported

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,9 +1,14 @@
+import { faker } from '@faker-js/faker'
 import { Product} from './product.model'
 import { CreateProductDto } from './product.dto'
 
 export const product: Product[] = []
 
 export const addProduct = (data: CreateProductDto) => {
+    const newProduct = {
+        ...data,
+        id:
+    }
     product.push(data)
 }
 
