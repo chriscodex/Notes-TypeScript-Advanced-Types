4be1f9f5fe017761e2df9fac9f3bc50a29e7f0df
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-09 22:37:02 -0500

    addProduct in for loop added

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,25 +1,29 @@
 import { faker } from '@faker-js/faker'
 import { addProduct, product } from './products/product.service'
 
-addProduct({
-    id: faker.datatype.uuid(),
-    description: faker.commerce.productDescription(),
-    image: faker.image.imageUrl(),
-    color: faker.commerce.color(),
-    price: parseInt(faker.commerce.price(), 10),
-    isNew: faker.datatype.boolean(),
-    tags: faker.helpers.arrayElements(),
-    title: faker.commerce.productName(),
-    createdAt: faker.date.recent(),
-    updatedAt: faker.date.recent(),
-    stock: faker.datatype.number({min: 10, max: 100}),
-    size: faker.helpers.arrayElement(['M','S','XL','L']),
-    category: {
+for (let index=0;index<50;index++){
+    addProduct({
         id: faker.datatype.uuid(),
+        description: faker.commerce.productDescription(),
+        image: faker.image.imageUrl(),
+        color: faker.commerce.color(),
+        price: parseInt(faker.commerce.price(), 10),
+        isNew: faker.datatype.boolean(),
+        tags: faker.helpers.arrayElements(),
+        title: faker.commerce.productName(),
         createdAt: faker.date.recent(),
         updatedAt: faker.date.recent(),
-        name: faker.commerce.productName()
-    }
-})
+        stock: faker.datatype.number({min: 10, max: 100}),
+        size: faker.helpers.arrayElement(['M','S','XL','L']),
+        category: {
+            id: faker.datatype.uuid(),
+            createdAt: faker.date.recent(),
+            updatedAt: faker.date.recent(),
+            name: faker.commerce.productName()
+        }
+    })
+}
+
+
 
 console.log(product)
