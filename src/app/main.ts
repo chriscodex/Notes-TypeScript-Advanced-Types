903c43006add3ffd8aa56f7efc6e527f4d6753da
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-09 19:27:14 -0500

    tag property added

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,19 +1,23 @@
-import {faker} from '@faker-js/faker'
-import {Product} from './products/product.model'
+import { faker } from '@faker-js/faker'
+import { addProduct } from './products/product.service'
 
-const p1: Product = {
+addProduct({
     id: faker.datatype.uuid(),
+    description: faker.commerce.productDescription(),
+    image: faker.image.imageUrl(),
+    color: faker.commerce.color(),
+    price: parseInt(faker.commerce.price(), 10),
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.arrayElement(),
     title: faker.commerce.productName(),
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    stock: 4,
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    stock: faker.datatype.number({min: 10, max: 100}),
     size: 'S',
     category: {
-        id: '1',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        name: 'clothes'
+        id: faker.datatype.uuid(),
+        createdAt: faker.date.recent(),
+        updatedAt: faker.date.recent(),
+        name: faker.commerce.productName()
     }
-}
-
-console.log(p1)
+})
