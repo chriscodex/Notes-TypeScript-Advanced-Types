cac77098a8595a6c916c7fae3bb88304e8ee7c0d
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-11 02:31:30 -0500

    variable using omit created

--- src/10-utility-types.ts
+++ src/10-utility-types.ts
@@ -9,3 +9,7 @@ interface EmployeeInterface {
 // Permite omitir propiedades a las interfaces
 type Person123 = Omit<EmployeeInterface,'company' | 'age'>
 
+const ppp: Person123 = {
+    name: 'Christian',
+    lastName: 'Espinoza',
+}
