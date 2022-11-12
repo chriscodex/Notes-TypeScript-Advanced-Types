03d4981ff5b94fefba2cfd82dc23ce5a36aa0bf8
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-12 03:45:29 -0500

    id changed to string

--- src/app/base.model.ts
+++ src/app/base.model.ts
@@ -1,5 +1,5 @@
 export interface BaseModel {
-    readonly id: string | number
+    readonly id: string
     readonly createdAt: Date
     updatedAt: Date
 }
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker'
-import { addProduct, products } from './products/product.service'
+import { addProduct, products, updateProduct } from './products/product.service'
 
 for (let index=0;index<50;index++){
     addProduct({
@@ -19,3 +19,8 @@ for (let index=0;index<50;index++){
 console.log(products)
 
 const product = products[0]
+
+updateProduct(product.id, {
+    title: 'New Title',
+    stock: 80
+})
